8c48649a8bc9bfc173cad39317eee1d87ede8d0e
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-05 09:36:30 +0545

    implementing MigLayout

--- lovecalculator/src/lovecalculator/Lovecalculator.java
+++ lovecalculator/src/lovecalculator/Lovecalculator.java
@@ -5,17 +5,69 @@
  */
 package lovecalculator;
 
+import java.awt.Color;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import net.miginfocom.swing.MigLayout;
+
+
 /**
  *
- * @author YoLo
- */  
+ * @author Mukesh Mahara
+ */
 public class Lovecalculator {
+	
+	private JFrame frame = new JFrame();
+	private JPanel contentpanel =new JPanel(new MigLayout("insets 300, center"));
+	private JButton submitButton = new JButton("Submit");
+	private JLabel textJLabel = new JLabel("Hello");
+	private JLabel textJLabel1 = new JLabel("world");
+	
+	public Lovecalculator() {
+		// TODO Auto-generated constructor stub
+		
+		frame.setTitle("Love Calculator");
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+		
+		
+		
+		contentpanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+
+		
+		contentpanel.add(textJLabel,"span 2 2, w 100!");
+		
+		contentpanel.add(textJLabel1,"span 0 1, w 100!");
+		
+		
+		contentpanel.add(submitButton,"w 100!");
+		
+		
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		frame.setLocationRelativeTo(contentpanel);
+	
+		frame.add(contentpanel);
+	
+		frame.setVisible(true);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+	
+	public static void main(String[] args) {
+		
+		new Lovecalculator();
+	}
+	
+	
+	
+	
+	
 }
