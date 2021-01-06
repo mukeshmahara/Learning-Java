33557c2bfeaec3cdc6f3d0dc0235deba1f33d8a4
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-06 07:33:41 +0545

    Learning Spring Boot Framework

---
+++ Tracker/src/test/java/io/javaSpringBoot/Tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javaSpringBoot.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
