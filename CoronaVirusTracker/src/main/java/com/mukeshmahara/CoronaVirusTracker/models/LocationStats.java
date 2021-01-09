3c04a9c86391f895a03740f303ee72b8b56e4df2
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-09 23:51:27 +0545

    model

--- CoronaVirusTracker/src/main/java/com/mukeshmahara/CoronaVirusTracker/models/LocationStats.java
+++ CoronaVirusTracker/src/main/java/com/mukeshmahara/CoronaVirusTracker/models/LocationStats.java
@@ -1,2 +1,55 @@
-package com.mukeshmahara.CoronaVirusTracker.models;public class LocationStats {
+package com.mukeshmahara.CoronaVirusTracker.models;
+
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int caseChange;
+
+    public int getCaseChange() {
+        return caseChange;
+    }
+
+    public void setCaseChange(int caseChange) {
+        this.caseChange = caseChange;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                ", prevDayCases=" + caseChange +
+                '}';
+    }
 }
