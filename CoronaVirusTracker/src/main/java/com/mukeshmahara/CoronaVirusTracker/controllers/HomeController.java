fa73588c68ce201014cf3132651f63dd4e5bfd4f
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-09 23:50:39 +0545

    home controller.java

--- CoronaVirusTracker/src/main/java/com/mukeshmahara/CoronaVirusTracker/controllers/HomeController.java
+++ CoronaVirusTracker/src/main/java/com/mukeshmahara/CoronaVirusTracker/controllers/HomeController.java
@@ -1,2 +1,30 @@
-package com.mukeshmahara.CoronaVirusTracker.controllers;public class HomeController {
+package com.mukeshmahara.CoronaVirusTracker.controllers;
+
+import com.mukeshmahara.CoronaVirusTracker.Services.CoronaVirusDataService;
+import com.mukeshmahara.CoronaVirusTracker.models.LocationStats;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getCaseChange()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases );
+        model.addAttribute("totalNewCases",totalNewCases );
+        return "home";
+    }
 }
