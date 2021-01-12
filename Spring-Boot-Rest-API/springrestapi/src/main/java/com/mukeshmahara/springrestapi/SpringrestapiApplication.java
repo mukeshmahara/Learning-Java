3ca59e30ad89855600e171326097ecf5c2130e6b
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-12 07:17:52 +0545

    main Application

---
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/SpringrestapiApplication.java
@@ -0,0 +1,14 @@
+package com.mukeshmahara.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestapiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringrestapiApplication.class, args);
+    }
+
+}
