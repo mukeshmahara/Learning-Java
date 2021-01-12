a0ad77cd9f942c5b7219524a837ba76e9fd1e7ed
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-12 11:27:12 +0545

    bookrepo interface added

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/dao/BookRepo.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/dao/BookRepo.java
@@ -1,7 +1,9 @@
 package com.mukeshmahara.springrestapi.dao;
 
 import com.mukeshmahara.springrestapi.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepo extends CrudRepository<Book,Integer> {
+public interface BookRepo extends JpaRepository<Book,Integer> {
 }
