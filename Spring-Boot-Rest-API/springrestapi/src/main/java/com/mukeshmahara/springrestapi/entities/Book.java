b92bffe3a7cc8f89840f4ea7bddd8aca27eb6e8c
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-12 07:21:54 +0545

    Book Entity class

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/entities/Book.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/entities/Book.java
@@ -1,2 +1,70 @@
-package com.mukeshmahara.springrestapi.entities;public class book {
+package com.mukeshmahara.springrestapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int book_id;
+    private String book_name;
+    private String publication;
+    @Autowired
+    @Embedded
+    private Author author;
+
+//    public Book(int book_id,String book_name,String publication,Author author) {
+//        this.book_id = book_id;
+//        this.book_name = book_name;
+//        this.publication = publication;
+//        this.author = author;
+//    }
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getPublication() {
+        return publication;
+    }
+
+    public void setPublication(String publication) {
+        this.publication = publication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "book_id=" + book_id +
+                ", book_name='" + book_name + '\'' +
+                ", publication='" + publication + '\'' +
+                ", author=" + author +
+                '}';
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
