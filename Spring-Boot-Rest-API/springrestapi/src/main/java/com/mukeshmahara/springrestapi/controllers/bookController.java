b855ad3c8599dd72724a7bfee66766dd72b36cd7
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-12 07:24:16 +0545

    Spring REST API and web Services MVC Pattern

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/controllers/bookController.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/controllers/bookController.java
@@ -1,2 +1,34 @@
-package com.mukeshmahara.springrestapi.controllers;public class bookController {
+package com.mukeshmahara.springrestapi.controllers;
+
+
+import com.mukeshmahara.springrestapi.entities.Book;
+import com.mukeshmahara.springrestapi.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class bookController {
+    @Autowired
+    BookService bookService;
+
+    @GetMapping("/getbooks")
+    public List getbooks(){  //It will get all the books we have
+        return this.bookService.getbooks();
+    }
+
+    @GetMapping("/getbook/{book_id}")
+    public Book getbook(@PathVariable String book_id){
+        return bookService.getbook(Long.parseLong(book_id));
+    }
+
+    @GetMapping(path="/books", consumes = "application/json")
+        public Book addbooks(@RequestBody Book book){
+        return this.bookService.addbooks(book);
+        }
+
 }
--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/services/BookService.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/services/BookService.java
@@ -1,2 +1,11 @@
-package com.mukeshmahara.springrestapi.services;public interface BookService {
+package com.mukeshmahara.springrestapi.services;
+
+import com.mukeshmahara.springrestapi.entities.Book;
+
+import java.util.List;
+
+public interface BookService{
+    public List<Book> getbooks();
+    public Book getbook(long book_id);
+    public Book addbooks(Book book);
 }
