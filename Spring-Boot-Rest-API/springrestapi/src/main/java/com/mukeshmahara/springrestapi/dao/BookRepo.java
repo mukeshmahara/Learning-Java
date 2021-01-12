5a95c9e98e60ecda9b927c00cbc25ce56bdc19f9
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-12 07:19:27 +0545

    gitrepo interface

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/dao/BookRepo.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/dao/BookRepo.java
@@ -1,2 +1,7 @@
-package com.mukeshmahara.springrestapi.dao;public interface BookRepo {
+package com.mukeshmahara.springrestapi.dao;
+
+import com.mukeshmahara.springrestapi.entities.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepo extends CrudRepository<Book,Integer> {
 }
