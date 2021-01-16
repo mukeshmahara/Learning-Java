4ca5e4f977535e97b92b15fdc2dfb835140f7116
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-16 09:51:25 +0545

    Controller for index

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/controllers/indexController.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/controllers/indexController.java
@@ -1,18 +1,18 @@
 package com.mukeshmahara.springrestapi.controllers;
 
-import com.mukeshmahara.springrestapi.entities.Book;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class indexController {
 
     @RequestMapping("/")
-    public String home(){
+    public ModelAndView home(){
+        System.out.println("this is form index controller");
+        ModelAndView m = new ModelAndView();
 
-        return "index.html";
+        return m;
     }
 }
