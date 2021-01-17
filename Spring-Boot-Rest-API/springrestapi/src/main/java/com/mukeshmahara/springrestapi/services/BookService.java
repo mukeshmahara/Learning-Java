4d05bef7247e12835b4db8b01105df858b8b2d7c
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-17 10:25:27 +0545

    updated class files

--- Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/services/BookService.java
+++ Spring-Boot-Rest-API/springrestapi/src/main/java/com/mukeshmahara/springrestapi/services/BookService.java
@@ -4,8 +4,14 @@ import com.mukeshmahara.springrestapi.entities.Book;
 
 import java.util.List;
 
-public interface BookService{
+public interface BookService {
     public List<Book> getbooks();
+
     public Book getbook(long book_id);
+
     public Book addbooks(Book book);
+
+    Book deletebook(int book_id);
+
+    Book updatebook(Book book);
 }
