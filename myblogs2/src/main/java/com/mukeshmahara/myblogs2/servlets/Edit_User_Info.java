8eb12597a75b3fffc801b61a844fe0edb0cd15ac
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-28 14:58:03 +0545

    Servlet Configuration files

---
+++ myblogs2/src/main/java/com/mukeshmahara/myblogs2/servlets/Edit_User_Info.java
@@ -0,0 +1,16 @@
+package com.mukeshmahara.myblogs2.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EditServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+    
+
+    }
+}
