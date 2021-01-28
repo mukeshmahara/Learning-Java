d2eb94acb6e5d9a9e86fa2e3b6754fa85c8e7fba
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-28 16:10:55 +0545

    helper class

---
+++ myblogs2/src/main/java/com/mukeshmahara/myblogs2/helper/ConnectionProvider.java
@@ -0,0 +1,26 @@
+package com.mukeshmahara.myblogs2.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+    private static Connection con;
+
+    public static Connection getCon() {
+        if (con == null) {
+
+            try {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myblogs","root","password");
+
+            } catch (Exception e) {
+                System.out.println("Unable to load Driver");
+                e.printStackTrace();
+            }
+        }
+        
+        
+        return con;
+    }
+}
