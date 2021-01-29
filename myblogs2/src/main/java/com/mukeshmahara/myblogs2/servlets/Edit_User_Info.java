2a5d8da3d64d4009d0973884ea914d16998c1bff
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-01-29 10:18:17 +0545

    creadted editable user profile info

--- myblogs2/src/main/java/com/mukeshmahara/myblogs2/servlets/Edit_User_Info.java
+++ myblogs2/src/main/java/com/mukeshmahara/myblogs2/servlets/Edit_User_Info.java
@@ -1,16 +1,48 @@
 package com.mukeshmahara.myblogs2.servlets;
 
+import com.mukeshmahara.myblogs2.dao.UserDao;
+import com.mukeshmahara.myblogs2.entities.User;
+import com.mukeshmahara.myblogs2.helper.ConnectionProvider;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.*;
 import java.io.IOException;
 
-public class EditServlet extends HttpServlet {
+
+@MultipartConfig
+public class Edit_User_Info extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doPost(req, resp);
-    
+        resp.setContentType("text/html");
 
+//Fetching all Updated users data
+        Part part = req.getPart("user_image");
+        String image_name = part.getSubmittedFileName();
+
+        String email = req.getParameter("user_email");
+        String phone = req.getParameter("user_phone");
+        String address = req.getParameter("user_address");
+        System.out.println(image_name);
+
+        HttpSession session = req.getSession();
+
+        User user = (User) session.getAttribute("current_user");
+
+        user.setEmail(email);
+        user.setPhone(phone);
+        user.setAddress(address);
+        user.setUser_image(image_name);
+
+        UserDao dao = new UserDao(ConnectionProvider.getCon());
+
+        boolean status = dao.updateUserInfo(user);
+        if (status) {
+            resp.getWriter().println("Data updated succesfully");
+        } else
+            resp.getWriter().println("Error occured while updating data");
+    resp.sendRedirect("profile.jsp");
     }
+
 }
