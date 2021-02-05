7f4ece5531d2a6e262172263b2855ab7ea69d6e9
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-02-05 22:39:50 +0545

    inserting data to database using spring JDBC template

--- springcore/src/main/java/org/mukeshmahara/App.java
+++ springcore/src/main/java/org/mukeshmahara/App.java
@@ -2,15 +2,24 @@ package org.mukeshmahara;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
-        Student s =(Student)context.getBean("student1");
-        System.out.println(s.toString());
+        ApplicationContext   context = new ClassPathXmlApplicationContext("app-context.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+
+//        insert query
+
+        String sql = "insert into student(id,name,city) values(?,?,?)";
+
+//        firing the query
+
+        int result = template.update(sql, 2, "Suraj2", "Kanchanpur2");
+        System.out.println("number of record inserted :"+result);
     }
 }
