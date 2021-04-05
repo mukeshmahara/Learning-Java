615aa49d83784171b28babdece2a17a2f0b3b08a
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-04-05 21:53:56 +0545

    ExpenseService

--- Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/model/Expense.java
+++ Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/model/Expense.java
@@ -1,2 +1,40 @@
-package com.mukeshmahara.expensetracker.model;public class Expense {
+package com.mukeshmahara.expensetracker.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+//@Table(name = "Expenses")
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int expenseID;
+//    @Column(name = "expensetype")
+    private String expenseType;
+    private Date expenseDate;
+    private int moneySpent;
+
+    public String getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(String expenseType) {
+        this.expenseType = expenseType;
+    }
+
+    public Date getExpenseDate() {
+        return expenseDate;
+    }
+
+    public void setExpenseDate(Date expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+
+    public int getMoneySpent() {
+        return moneySpent;
+    }
+
+    public void setMoneySpent(int moneySpent) {
+        this.moneySpent = moneySpent;
+    }
 }
--- Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/service/ExpenseService.java
+++ Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/service/ExpenseService.java
@@ -1,4 +1,12 @@
 package com.mukeshmahara.expensetracker.service;
 
+import com.mukeshmahara.expensetracker.model.Expense;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface ExpenseService {
+    List<Expense> getAllExpense();
+    boolean addExpense(Expense expense);
 }
