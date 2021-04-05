ff2be79fcf7455bb32f2275ba5ecc01d2a14cf35
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-04-05 21:49:14 +0545

    Expense Service

--- Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/service/ExpenseServiceImpl.java
+++ Spring boot Project/src/main/java/com/mukeshmahara/expensetracker/service/ExpenseServiceImpl.java
@@ -1,2 +1,36 @@
-package com.mukeshmahara.expensetracker.service;public class ExpenseServiceImpl {
+package com.mukeshmahara.expensetracker.service;
+
+import com.mukeshmahara.expensetracker.model.Expense;
+import com.mukeshmahara.expensetracker.repository.ExpenseRepository;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseServiceImpl implements ExpenseService{
+//    @Autowired
+//    private SessionFactory sessionFactory;
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @Override
+    public List<Expense> getAllExpense() {
+    List<Expense> list= expenseRepository.findAll();
+        return list;
+    }
+
+    @Override
+    public boolean addExpense(Expense expense) {
+        boolean b= false;
+
+        if(expense!=null){
+            expenseRepository.save(expense);
+            System.out.println("Expense added successfully");
+            b=true;
+        }
+        return b;
+    }
 }
